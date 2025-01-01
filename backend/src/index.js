14417f311b61a906fc1bb3e9656884d6ebee4da4
zZz-ren <109126118+zZz-ren@users.noreply.github.com>
2025-01-01 18:16:52 +0530

    Update index.js added optional PORT

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ var db_1 = require("./lib/db");
 var dotenv_1 = require("dotenv");
 var socket_io_1 = require("./lib/socket.io");
 dotenv_1.default.config();
-socket_io_1.server.listen(process.env.PORT, function () {
-    console.log("server started on http://locahost:", process.env.PORT);
+socket_io_1.server.listen(process.env.PORT || 5001, function () {
+    console.log("server started on http://locahost:", process.env.PORT || 5001);
     (0, db_1.connectDB)();
 });
